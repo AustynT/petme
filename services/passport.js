388208052367227b61dd6a91e68801764710978b
AustynT <Austyntravis9@gmail.com>
2019-01-15 16:31:15 -0700

    built local startegy

--- services/passport.js
+++ services/passport.js
@@ -1,2 +1,30 @@
 const passport = require("passport");
 const User = require("../models/User");
+const config = require("../config");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const LocalStrategy = require("passport-local");
+
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    User.findOne({ email: email }, function(err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false);
+      }
+
+      user.comparePassword(password, function(err, isMatch) {
+        if (err) {
+          return done(err);
+        }
+        if (!isMatch) {
+          return done(null, false);
+        }
+
+        return done(null, user);
+      });
+    });
+  })
+);
