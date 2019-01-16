ab13c24676227dc7359e5faf98658a7e4879bfb3
AustynT <Austyntravis9@gmail.com>
2019-01-15 17:01:33 -0700

    added bycrypt, used pre to save password, and made comparePassword

--- models/User.js
+++ models/User.js
@@ -1,11 +1,41 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt-nodejs");
 
 const UserSchema = new Schema({
   fullName: String,
   email: String,
   password: String
 });
+
+UserSchema.pre("save", function(next) {
+  const user = this;
+
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(loginPassword, this.password, function(err, isMatch) {
+    if (err) {
+      return callback(err);
+    }
+
+    callback(null, isMatch);
+  });
+};
 
 const ModelClass = mongoose.model("user", UserSchema);
 
