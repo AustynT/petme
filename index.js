9774c1bdb77bd5ee4e0b883eb1fe09a0e28a9bec
AustynT <Austyntravis9@gmail.com>
2019-01-16 23:17:51 -0700

    added auth routes, added mongo, and body parser

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express"); // adds express library to the file
+
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const config = require("./config");
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect("mongodb://localhost/petme_database");
+
 const app = express(); // calling express like a function it generates a new application
 
+app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send({ home: "page" });
 });
 
+require("./routes/authRoutes")(app);
 const PORT = process.env.PORT || 5000;
 
 app.listen(5000);
